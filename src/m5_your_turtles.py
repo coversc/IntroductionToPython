73620bcaccb3315b54ba6b9c5969c0b8f1492b10
coversc <coversc@rose-hulman.edu>
2017-11-28 17:45:34 -0500

    I added my name

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Caitlin Coverstone.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,50 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+turtle_1 = rg.SimpleTurtle('turtle')
+turtle_1.pen = rg.Pen('yellow', 2)
+turtle_1.speed = 20
+
+size = 15
+for k in range(20):
+    turtle_1.draw_circle(size)
+
+    # Move a little below and to the right of where the previous
+    # square started.  Do this with the pen up (so nothing is drawn).
+    turtle_1.pen_up()
+    turtle_1.right(45)
+    turtle_1.forward(10)
+    turtle_1.left(45)
+
+    # Put the pen down again (so drawing resumes).
+    # Make the size for the NEXT square be 12 pixels smaller.
+    turtle_1.pen_down()
+    size = size - 12
+
+turtle_2 = rg.SimpleTurtle('turtle')
+turtle_2.pen = rg.Pen('blue', 2)
+turtle_2.speed = 100
+
+#size = 150
+for k in range(25):
+    turtle_2.draw_regular_polygon(8,30)
+
+    # Move a little below and to the right of where the previous
+    # square started.  Do this with the pen up (so nothing is drawn).
+    turtle_2.pen_down()
+    turtle_2.right(45)
+    turtle_2.forward(10)
+    turtle_2.left(45 + k)
+
+    # Put the pen down again (so drawing resumes).
+    # Make the size for the NEXT square be 12 pixels smaller.
+    turtle_2.pen_down()
+    size = size - 12
+
+window.close_on_mouse_click()
+
